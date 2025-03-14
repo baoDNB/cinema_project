946aa8df19b8329378bf627c54ec4a70b5974c95
Bảo <126046620+baoDNB@users.noreply.github.com>
2025-03-14 23:59:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup>
+import DefaultComponent from './Components/DefaultComponent/Default.vue';
+</script>
+
+<template >
+  <DefaultComponent />
+
+</template>
---
+++ src/Components/DefaultComponent/Default.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="app-container">
+        <HeaderComponent />
+        <main class="content">
+            <router-view />
+        </main>
+        <FooterComponent />
+    </div>
+</template>
+
+<style>
+.app-container {
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    /* Chiều cao ít nhất bằng 100% màn hình */
+}
+
+.content {
+   
+    flex: 1;
+    /* Phần nội dung mở rộng để đẩy Footer xuống */
+}
+</style>
+
+
+<script>
+import HeaderComponent from "../HeaderComponent/HeaderComponent.vue";
+import FooterComponent from "../FooterComponent/FooterComponent.vue";
+
+export default {
+    components: {
+        HeaderComponent,
+        FooterComponent,
+    },
+};
+</script>
